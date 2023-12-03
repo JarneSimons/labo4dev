993a5a4c7931c3295343f7915952ccdd9f94c740
JarneSimons <r0901961@student.thomasmore.be>
2023-12-03 19:53:22 +0100

    feat: add routes folder with messages api

---
+++ routers/api/v1/messages.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const controllerMessages = require('../../../controllers/api/v1/messages')
+
+// get api/v1/messages from mongodb
+router.get("/", controllerMessages.getMessages);
+
+// get api/v1/messages/:id from mongodb
+router.get('/:id', controllerMessages.getMessageId)
+  
+// post api/v1/messages to mongodb
+router.post('/', controllerMessages.postMessages)
+
+// put api/v1/messages/:id to mongodb
+router.put('/:id', controllerMessages.putMessagesId)
+
+// delete api/v1/messages/:id from mongodb
+router.delete('/:id', controllerMessages.deleteMessagesId)
+
+// get api/v1/messages/:user from mongodb
+router.get('/:user', controllerMessages.getMessagesByUser)
+
+
+module.exports = router;
