4ad766f108d0c9605f50c3282313fb0d128b5d07
JarneSimons <r0901961@student.thomasmore.be>
2023-12-04 14:53:27 +0100

    Update MongoDB connection string

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routerMessages = require('./routers/api/v1/messages')
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/messages', 
+mongoose.connect('process.env.MONGODB', 
 {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
