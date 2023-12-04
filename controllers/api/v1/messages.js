3c021ccc6d705cdaf51c0055a176e7354ec36546
JarneSimons <r0901961@student.thomasmore.be>
2023-12-04 12:23:40 +0100

    Fix: message object properties

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -81,8 +81,8 @@ const postMessages = async (req, res) => {
 
         // Create a new Message instance with data from the request body
         let message = new Messages();
-        message.user = req.body.user;
-        message.text = req.body.text;
+        message.user = req.body.message.user;
+        message.text = req.body.message.text;
 
         // Save the message to the database
         const savedMessage = await message.save();
